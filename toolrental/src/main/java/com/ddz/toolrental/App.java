125c3ef62fa5e23c7c3c9f3ce633c03962c4a6d2
Dante <dzite05@acd.ccac.edu>
2021-06-22 16:30:35 -0400

    Added basic driver program

--- toolrental/src/main/java/com/ddz/toolrental/App.java
+++ toolrental/src/main/java/com/ddz/toolrental/App.java
@@ -1,7 +1,14 @@
 package com.ddz.toolrental;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import com.ddz.toolrental.core.RentalAgreement;
+import com.ddz.toolrental.services.CheckoutService;
+import com.ddz.toolrental.services.ToolRepositoryLoader;
+
 /**
- *  Dummy driver class
+ *  Basic driver class for demonstration
  * @author Dante Zitello
  *
  */
@@ -9,6 +16,25 @@ public class App
 {	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	try {
+	    	ToolRepositoryLoader loaderService = ToolRepositoryLoader.getRepositoryLoader();
+			loaderService.loadRepositories("resources/xsd/tooltype.xsd",
+					   "resources/xsd/tool.xsd",
+					   "resources/tooltypes",
+					   "resources/tools");
+			
+			CheckoutService svc = CheckoutService.getCheckoutService();
+	    	
+	    	
+	    	
+	    	RentalAgreement lag = svc.checkoutTool(
+					"JAKR", 50, LocalDate.parse("2020-07-02"), 4);
+			
+			
+			System.out.println(lag.buildAgreementString());
+    	}
+    	catch(Exception e) {
+    		e.printStackTrace();
+    	}
     }
 }
